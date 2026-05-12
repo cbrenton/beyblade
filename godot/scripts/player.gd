3e0f2cc0ea70821c20bf942f73eb96015a6cd7c1
Chris Brenton <hiring@rubyridge.org>
2026-05-12 16:25:44 -0400

    add win sounds

--- godot/scripts/player.gd
+++ godot/scripts/player.gd
@@ -11,6 +11,8 @@ var should_log = false
 @onready var fuck_with_timer := Timer.new()
 @onready var collision_player = get_parent().get_parent().get_node("CollisionPlayer")
 
+signal die
+
 
 func _ready() -> void:
     var n := randi() % 42 + 1
@@ -22,51 +24,44 @@ func _ready() -> void:
 
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-    for i in state.get_contact_count():
-        var collider = state.get_contact_collider_object(i)
-        if collider == null:
-            continue
-        var normal := state.get_contact_local_normal(i)
-        var impact := absf(state.linear_velocity.dot(normal))
-
-        if collider is StaticBody2D:
-            var drain := impact * wall_spin_drain
-            state.angular_velocity -= signf(state.angular_velocity) * drain
-            animation.play("hit")
-        elif collider is RigidBody2D:
-            var other := collider as RigidBody2D
-            var my_speed := absf(state.angular_velocity)
-            var their_speed := absf(other.angular_velocity)
-            var total := my_speed + their_speed
-            if total == 0.0:
+    if is_live:
+        for i in state.get_contact_count():
+            var collider = state.get_contact_collider_object(i)
+            if collider == null:
                 continue
-            var my_loss := total * top_spin_drain * (their_speed / total)
-            state.angular_velocity -= signf(state.angular_velocity) * my_loss
-            var away := (global_position - other.global_position).normalized()
-            var scaler = 2.0
-            state.linear_velocity += away * collision_knockback * (their_speed / my_speed) * scaler
-            animation.play("hit")
-            print("hit player")
-            collision_player.play()
+            var normal := state.get_contact_local_normal(i)
+            var impact := absf(state.linear_velocity.dot(normal))
+
+            if collider is StaticBody2D:
+                var drain := impact * wall_spin_drain
+                state.angular_velocity -= signf(state.angular_velocity) * drain
+                animation.play("hit")
+            elif collider is RigidBody2D:
+                var other := collider as RigidBody2D
+                var my_speed := absf(state.angular_velocity)
+                var their_speed := absf(other.angular_velocity)
+                var total := my_speed + their_speed
+                if total == 0.0:
+                    continue
+                var my_loss := total * top_spin_drain * (their_speed / total)
+                state.angular_velocity -= signf(state.angular_velocity) * my_loss
+                var away := (global_position - other.global_position).normalized()
+                var scaler = 10.0
+                state.linear_velocity += (
+                    away * collision_knockback * (their_speed / my_speed) * scaler
+                )
+                animation.play("hit")
+                print("hit player")
+                collision_player.play()
 
 
 func _physics_process(_delta: float) -> void:
-    var to_center = Vector2.ZERO - position
-    if to_center.length() > 50.0:
-        apply_central_force(to_center.normalized() * pull_factor)
-    if should_log:
-        print("position:", position)
-
-
-func _input(event: InputEvent) -> void:
-    if event is InputEventKey and event.pressed:
-        if event.keycode == KEY_F:
-            apply_torque_impulse(1000)
-        if event.keycode == KEY_A:
-            apply_central_impulse(Vector2(-1.0, 0.0))
-
-        if event.keycode == KEY_D:
-            apply_central_impulse(Vector2(1.0, 0.0))
+    if is_live:
+        var to_center = Vector2.ZERO - position
+        if to_center.length() > 50.0:
+            apply_central_force(to_center.normalized() * pull_factor)
+        if should_log:
+            print("position:", position)
 
 
 func launch(initial_spin: float) -> void:
@@ -85,6 +80,11 @@ func _process(_delta: float) -> void:
         is_live = false
         get_node("Sprite2D").modulate = Color.BLACK
         print("u lose")
+        die.emit()
+    if is_live && !get_viewport_rect().has_point(global_position):
+        print("OFFSCREEN")
+        is_live = false
+        die.emit()
 
 
 func set_color(color: Color) -> void:
